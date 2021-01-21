cdfcc95d039cd5ca4cb357a93b65372cda1ec23d
hks <sutt2000@naver.com>
2021-01-22 03:01:45 +0900

    04 턴 바꾸기 버튼에 disable적용

--- js/scripts1.js
+++ js/scripts1.js
@@ -1,7 +1,12 @@
 var comScore = 0;
-var useScore = 0;
+var userScore = 0;
+var isComputerTurn = true;
 
 function onComputerShoot() {
+  if (!isComputerTurn) {
+    return;
+  }
+
   var textElem = document.getElementById("text");
   var shootType = Math.random() < 0.5 ? 2 : 3;
 
@@ -24,28 +29,56 @@ function onComputerShoot() {
       textElem.innerHTML = "컴퓨터가 3점슛을 실패했습니다..";
     }
   }
+  isComputerTurn = false;
+
+  var computerButtons = document.getElementsByClassName("btn-computer");
+
+  for (let i = 0; i < computerButtons.length; i++) {
+    computerButtons[i].disabled = true;
+  }
+
+  var userButtons = document.getElementsByClassName("btn-user");
+
+  for (let i = 0; i < userButtons.length; i++) {
+    userButtons[i].disabled = false;
+  }
 }
 
-function onUserShoot() {
+function onUserShoot(shootType) {
+  if (isComputerTurn) {
+    return;
+  }
   var textElem = document.getElementById("text");
-  var shootType = Math.random() < 0.5 ? 2 : 3;
+  var userScorElem = document.getElementById("user-score");
 
   if (shootType === 2) {
     if (Math.random() < 0.5) {
       textElem.innerHTML = "2점슛을 성공 시켰습니다.";
-      useScore += 2;
-      var comScorElem = document.getElementById("user-score");
-      comScorElem.innerHTML = useScore;
+      userScore += 2;
+      userScorElem.innerHTML = userScore;
     } else {
       textElem.innerHTML = "2점슛을 실패했습니다..";
     }
   } else {
     if (Math.random() < 0.33) {
       textElem.innerHTML = "3점슛을 성공 시켰습니다.";
-      useScore += 3;
-      var comScorElem = document.getElementById("user-score");
+      userScore += 3;
+      userScorElem.innerHTML = userScore;
     } else {
       textElem.innerHTML = "3점슛을 실패했습니다..";
     }
+  }
+  isComputerTurn = true;
+
+  var userButtons = document.getElementsByClassName("btn-user");
+
+  for (let i = 0; i < userButtons.length; i++) {
+    userButtons[i].disabled = true;
+  }
+
+  var computerButtons = document.getElementsByClassName("btn-computer");
+
+  for (let i = 0; i < computerButtons.length; i++) {
+    computerButtons[i].disabled = false;
   }
 }
