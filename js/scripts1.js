12495223cbec28959de6ffcbc382a663c2f6f756
hks <sutt2000@naver.com>
2021-02-04 00:59:48 +0900

    농구AI 완성

--- js/scripts1.js
+++ js/scripts1.js
@@ -1,7 +1,16 @@
 var comScore = 0;
+var comPersent2 = 0.5;
+var comPersent3 = 0.33;
+
+
+
+
 var userScore = 0;
+var usetPrecent2 = 0.5;
+var usetPrecent3 = 0.33;
+
 var isComputerTurn = true;
-var shotsLeft = 3;
+var shotsLeft = 10;
 
 function showText(s) {
   var textElem = document.getElementById("text");
@@ -20,11 +29,46 @@ function updateUserScore(score) {
   userScoreElem.innerHTML = userScore;
 }
 
+function disableComputerButtons(flag) {
+  var computerButtons = document.getElementsByClassName("btn-computer");
+  for (let i = 0; i < computerButtons.length; i++) {
+    computerButtons[i].disabled = flag;
+  }
+}
+
+function disableUserButtons(flag) {
+  var userButtons = document.getElementsByClassName("btn-user");
+
+  for (let i = 0; i < userButtons.length; i++) {
+    userButtons[i].disabled = flag;
+  }
+}
+
+function updateAI() {
+  var diff = userScore - comScore;
+  if (diff >= 2) {
+    comPersent2 = 0.6;
+    comPersent3 = 0.44;
+  } else if (diff >= 5) {
+    comPersent3 = 0.7;
+    comPersent3 = 0.55;
+  } else if (diff <= -2) {
+    comPersent2 = 0.2;
+    comPersent3 = 0.22;
+  } else if (diff <= -5) {
+    comPersent2 = 0.1;
+    comPersent3 = 0.22;
+  }
+}
+
 function onComputerShoot() {
   if (!isComputerTurn) {
     return;
   }
-  var shootType = Math.random() < 0.5 ? 2 : 3;
+
+  updateAI();
+
+  var shootType = Math.random() < comPersent2 ? 2 : 3;
   if (shootType === 2) {
     if (Math.random() < 0.5) {
       showText("컴퓨터가 2점슛을 성공 시켰습니다.");
@@ -33,7 +77,7 @@ function onComputerShoot() {
       showText("컴퓨터가 2점슛을 실패했습니다..");
     }
   } else {
-    if (Math.random() < 0.33) {
+    if (Math.random() < comPersent3) {
       showText("컴퓨터가 3점슛을 성공 시켰습니다.");
       updateComputerScore(3);
     } else {
@@ -43,19 +87,9 @@ function onComputerShoot() {
 
   isComputerTurn = false;
 
-  var computerButtons = document.getElementsByClassName("btn-computer");
-
-  for (let i = 0; i < computerButtons.length; i++) {
-    computerButtons[i].disabled = true;
-  }
-
-  var userButtons = document.getElementsByClassName("btn-user");
-
-  for (let i = 0; i < userButtons.length; i++) {
-    userButtons[i].disabled = false;
-  }
+  disableComputerButtons(true);
+  disableUserButtons(false);
 }
-
 // ------------------------------------------------------------
 
 function onUserShoot(shootType) {
@@ -66,14 +100,14 @@ function onUserShoot(shootType) {
   var userScorElem = document.getElementById("user-score");
 
   if (shootType === 2) {
-    if (Math.random() < 0.5) {
+    if (Math.random() < usetPrecent2) {
       showText("2점슛을 성공 시켰습니다.");
       updateUserScore(2);
     } else {
       showText("2점슛을 실패했습니다..");
     }
   } else {
-    if (Math.random() < 0.33) {
+    if (Math.random() < usetPrecent3) {
       showText("3점슛을 성공 시켰습니다.");
       updateUserScore(3);
     } else {
@@ -83,17 +117,8 @@ function onUserShoot(shootType) {
 
   isComputerTurn = true;
 
-  var userButtons = document.getElementsByClassName("btn-user");
-
-  for (let i = 0; i < userButtons.length; i++) {
-    userButtons[i].disabled = true;
-  }
-
-  var computerButtons = document.getElementsByClassName("btn-computer");
-
-  for (let i = 0; i < computerButtons.length; i++) {
-    computerButtons[i].disabled = false;
-  }
+  disableComputerButtons(false);
+  disableUserButtons(true);
 
   shotsLeft--;
 
@@ -108,12 +133,9 @@ function onUserShoot(shootType) {
     } else {
       showText("비겼습니다.");
     }
-    for (let i = 0; i < computerButtons.length; i++) {
-      computerButtons[i].disabled = true;
-    }
-
-    for (let i = 0; i < userButtons.length; i++) {
-      userButtons[i].disabled = true;
-    }
+    // disableComputerButtons(false);
+    // disableUserButtons(true);
+    disableComputerButtons(true);
+    disableUserButtons(true);
   }
 }
